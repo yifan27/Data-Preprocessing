a184527ef442e01275eb39c0ecb16b041af83afe
yifan27 <30607277+yifan27@users.noreply.github.com>
2017-08-09 15:44:48 -0500

    Add files via upload

---
+++ Data Preprocessing.R
@@ -0,0 +1,44 @@
+# Data Preprocessing Template
+
+# Packages: stats, caTools
+# Important functions: 
+#         1.function(x): to set self function
+#         2.ifelse(test, yes, no)
+#         3.scale(x): Feature Scaling 
+
+
+# Importing the dataset
+dataset = read.csv('Data.csv')
+
+# Only pick cloumns 2:3 
+#dataset = dataset[.2:3]
+
+
+# Taking care of missing data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
+
+# Encoding categorical data
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c('No', 'Yes'),
+                           labels = c(0, 1))
+
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8, group = NULL)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[,2:3] = scale(training_set[,2:3])
+test_set[,2:3] = scale(test_set[,2:3])
